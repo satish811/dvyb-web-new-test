f7ad687a321d22a42cac36cfbc418c29c55cf054
DomsGlobal <domsgloballlp@gmail.com>
2026-05-01 00:50:41 +0530

    Change home banner image from hero_women.jpg to women.png

--- src/pages/b2c/homePage/homePage.jsx
+++ src/pages/b2c/homePage/homePage.jsx
@@ -1,7 +1,7 @@
 import CategoryCarousel from "../../../components/utils/CategoryCarousel";
 import ProductGrid from "../../../components/product/productGrid";
 import SectionTitle from "../../../components/utils/SectionTitle";
-import homeBannerWomen from "../../../assets/hero_women.jpg";
+import homeBannerWomen from "../../../assets/women.png";
 import homeBannerMen from "../../../assets/men.png";
 import ClosetIconsSection from "../../../components/b2c/home/ClosetIconsSection";
 import { useProducts } from "../../../hooks/useProducts";
